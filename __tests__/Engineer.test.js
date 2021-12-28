5c2c627d3611e3cb851ecb2a5abbff31daf550f7
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-12-27 19:15:46 -0600

    created Engineer Object and passed Engineer tests
    Also removed redundant test code in ./lib folder

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,24 @@
 // using Employee constructor 
-const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+//creates Engineer Object
+
+test('creates an Engineer Object', () =>{
+    const engineer = new Engineer('Dilbert','4213','fakeEmail@gmail.com','Arcanaut');
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+//get engineer's github info from getGithub()
+test('gets engineer github values',() => {
+    const engineer = new Engineer('Dilbert',4213,'fakeEmail@gmail.com','Arcanaut');
+
+    //expecting it with 'toEqual(expect.stringContaining' for confirmation
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+//gets engineer from getRole()
+test('get engineer role',() =>{
+    const engineer = new Engineer('Dilbert',4213,'fakeEmail@gmail.com','Arcanaut');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,10 @@
-// using Employee constructor 
+// imports parent constructor 'Employee' 
 const Employee = require('../lib/Employee');
+
+//engineer constructor inherits from parent constructor
+
+class Engineer extends Employee {
+    constructor (name, id, email, github){
+        //calls parent constructor 
+    }
+}
