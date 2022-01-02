2efbc653210949b801f3140d699704e7bd19ec38
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-01-01 23:03:46 -0600

    fixing merge conflict

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,8 +3,8 @@ const Employee = require('../lib/Employee');
 
 //engineer constructor inherits from parent constructor
 
-class Engineer extends Employee {
-    constructor (name, id, email, github){
-        //calls parent constructor 
-    }
-}
+//gets manager role from getRole()
+test('gets manager role', () => {
+    const manager = new Manager('Dilbert', 4213, 'fakeEmail@gmail.com', 4213);
+    expect(manager.getRole()).toEqual("Manager"); 
+});
