eebabb505eb07a63db7dcb6ffa0186654abfc1ef
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-01-05 18:21:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ const writeFile = data => {
             return;
             // when the profile has been created 
         } else {
-            console.log("Your team profile has been successfully created! Please check out the index.html")
+            console.log("Your team profile has been successfully created!")
         }
     })
 };
